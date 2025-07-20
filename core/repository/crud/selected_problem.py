354d01e049313a3c92623db50f196f58d64ea12c
Nikita <kuzliaev.nikita@yandex.ru>
2025-07-20 22:18:54 +0700

    feat(selected_problem): add get_selected_problem_of_contestant_by_id method in crud

--- core/repository/crud/selected_problem.py
+++ core/repository/crud/selected_problem.py
@@ -1,25 +1,28 @@
+from typing import Sequence, List
+
 from sqlalchemy import select, update
 
 from core.dependencies.repository import get_repository
-from core.models import QuizField
+from core.models import QuizField, SelectedProblem
 from core.repository.crud.base import BaseCRUDRepository
 
 
 class SelectedProblemCRUDRepository(BaseCRUDRepository):
 
-    async def get_selected_problem_by_id(
+    async def get_selected_problem_of_contestant_by_id(
             self,
-            quiz_field_id: int,
-    ) -> QuizField | None:
-        res = await self.async_session.execute(
-            select(
-                QuizField
-            )
-            .where(
-                QuizField.id == quiz_field_id,
-            )
+            contestant_id: int,
+            filter_by_status: List[str] = None,
+    ) -> Sequence[SelectedProblem]:
+        stmt = select(SelectedProblem).where(
+            SelectedProblem.contestant_id == contestant_id
         )
-        return res.scalar_one_or_none()
+
+        if filter_by_status:
+            stmt = stmt.where(SelectedProblem.status.in_(filter_by_status))
+
+        res = await self.async_session.execute(stmt)
+        return res.scalars().all()
 
 
 selected_problem_repo = get_repository(
