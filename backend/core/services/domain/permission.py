bf05f5082c447222c21c86969324974450ac2a79
Nikita <kuzliaev.nikita@yandex.ru>
2025-08-01 13:11:29 +0700

    refactor(permission): prepare for totally delete old permission logic realization

--- backend/core/services/domain/permission.py
+++ backend/core/services/domain/permission.py
@@ -22,6 +22,7 @@ from backend.core.schemas.permission import (
 )
 from backend.core.services.interfaces.permission import IPermissionService
 from backend.core.utilities.exceptions.database import EntityDoesNotExist
+from backend.core.utilities.exceptions.handlers.development import ObsoleteException
 from backend.core.utilities.exceptions.permission import PermissionDenied
 
 
@@ -30,16 +31,17 @@ class PermissionService(IPermissionService):
             self,
             uow: UnitOfWork,
     ):
-        self.uow = uow
+        raise ObsoleteException('Сервис PermissionService упразднен')
 
     async def raise_if_not_all(
             self,
             permissions: list[Callable[[], Awaitable[Permission | None]]],
     ) -> None:
-        for permission in permissions:
-            result = await permission()
-            if result is None:
-                raise PermissionDenied('Permission denied')
+        raise ObsoleteException
+        # for permission in permissions:
+        #     result = await permission()
+        #     if result is None:
+        #         raise PermissionDenied('Permission denied')
 
     async def create_permission(
             self,
@@ -48,19 +50,20 @@ class PermissionService(IPermissionService):
             permission_type: str,
             resource_id: Optional[int] = None,
     ) -> PermissionId:
-        async with self.uow:
-            permission: Permission = (
-                await self.uow.permission_repo.create_permission(
-                    user_id=user_id,
-                    resource_type=resource_type,
-                    resource_id=resource_id,
-                    permission_type=permission_type,
-                )
-            )
-            res = PermissionId(
-                permission_id=permission.id,
-            )
-            return res
+        raise ObsoleteException
+        # async with self.uow:
+        #     permission: Permission = (
+        #         await self.uow.permission_repo.create_permission(
+        #             user_id=user_id,
+        #             resource_type=resource_type,
+        #             resource_id=resource_id,
+        #             permission_type=permission_type,
+        #         )
+        #     )
+        #     res = PermissionId(
+        #         permission_id=permission.id,
+        #     )
+        #     return res
 
     async def delete_permission(
             self,
@@ -69,7 +72,7 @@ class PermissionService(IPermissionService):
             permission_type: str,
             resource_id: Optional[int] = None,
     ) -> None:
-        raise NotImplementedError('method not implemented')
+        raise ObsoleteException
 
     async def check_permission(
             self,
@@ -78,180 +81,189 @@ class PermissionService(IPermissionService):
             permission_type: str,
             resource_id: Optional[int] = None,
     ) -> PermissionId | None:
-        async with self.uow:
-            permission: Permission = (
-                await self.uow.permission_repo.check_permission(
-                    user_id=user_id,
-                    resource_type=resource_type,
-                    resource_id=resource_id if resource_id else user_id,
-                    permission_type=permission_type,
-                )
-            )
-            if not permission:
-                return None
-
-            res = PermissionId(
-                permission_id=permission.id,
-            )
-            return res
+        raise ObsoleteException
+        # async with self.uow:
+        #     permission: Permission = (
+        #         await self.uow.permission_repo.check_permission(
+        #             user_id=user_id,
+        #             resource_type=resource_type,
+        #             resource_id=resource_id if resource_id else user_id,
+        #             permission_type=permission_type,
+        #         )
+        #     )
+        #     if not permission:
+        #         return None
+        #
+        #     res = PermissionId(
+        #         permission_id=permission.id,
+        #     )
+        #     return res
 
     async def give_permission_for_admin_contest(
             self,
             user_id: int,
             contest_id: int,
     ) -> PermissionId:
-        res: PermissionId = (
-            await self.create_permission(
-                user_id=user_id,
-                resource_type=PermissionResourceType.CONTEST.value,
-                permission_type=PermissionActionType.ADMIN.value,
-                resource_id=contest_id,
-            )
-        )
-        return res
+        raise ObsoleteException
+        # res: PermissionId = (
+        #     await self.create_permission(
+        #         user_id=user_id,
+        #         resource_type=PermissionResourceType.CONTEST.value,
+        #         permission_type=PermissionActionType.ADMIN.value,
+        #         resource_id=contest_id,
+        #     )
+        # )
+        # return res
 
     async def check_permission_for_admin_contest(
             self,
             user_id: int,
             contest_id: int,
     ) -> PermissionId | None:
-        res: PermissionId = await self.check_permission(
-            user_id=user_id,
-            resource_type=PermissionResourceType.CONTEST.value,
-            permission_type=PermissionActionType.ADMIN.value,
-            resource_id=contest_id,
-        )
-        return res
+        raise ObsoleteException
+        # res: PermissionId = await self.check_permission(
+        #     user_id=user_id,
+        #     resource_type=PermissionResourceType.CONTEST.value,
+        #     permission_type=PermissionActionType.ADMIN.value,
+        #     resource_id=contest_id,
+        # )
+        # return res
 
     async def give_permission_for_edit_contest(
             self,
             user_id: int,
             contest_id: int,
     ) -> PermissionId:
-        res: PermissionId = (
-            await self.create_permission(
-                user_id=user_id,
-                resource_type=PermissionResourceType.CONTEST.value,
-                permission_type=PermissionActionType.EDIT.value,
-                resource_id=contest_id,
-            )
-        )
-        return res
+        raise ObsoleteException
+        # res: PermissionId = (
+        #     await self.create_permission(
+        #         user_id=user_id,
+        #         resource_type=PermissionResourceType.CONTEST.value,
+        #         permission_type=PermissionActionType.EDIT.value,
+        #         resource_id=contest_id,
+        #     )
+        # )
+        # return res
 
     async def check_permission_for_edit_contest(
             self,
             user_id: int,
             contest_id: int,
     ) -> PermissionId | None:
-        res: PermissionId = (
-            await self.check_permission(
-                user_id=user_id,
-                resource_type=PermissionResourceType.CONTEST.value,
-                permission_type=PermissionActionType.EDIT.value,
-                resource_id=contest_id,
-            )
-        )
-        return res
+        raise ObsoleteException
+        # res: PermissionId = (
+        #     await self.check_permission(
+        #         user_id=user_id,
+        #         resource_type=PermissionResourceType.CONTEST.value,
+        #         permission_type=PermissionActionType.EDIT.value,
+        #         resource_id=contest_id,
+        #     )
+        # )
+        # return res
 
     async def check_permission_for_edit_quiz_field(
             self,
             user_id: int,
             quiz_field_id: int,
     ) -> PermissionId | None:
-        async with self.uow:
-            quiz_field: QuizField = (
-                await self.uow.quiz_field_repo.get_quiz_field_by_id(
-                    quiz_field_id=quiz_field_id,
-                )
-            )
-            if not quiz_field:
-                return None
-
-            res: PermissionId | None = (
-                await self.check_permission_for_edit_contest(
-                    user_id=user_id,
-                    contest_id=quiz_field.contest_id,
-                )
-            )
-            return res
+        raise ObsoleteException
+        # async with self.uow:
+        #     quiz_field: QuizField = (
+        #         await self.uow.quiz_field_repo.get_quiz_field_by_id(
+        #             quiz_field_id=quiz_field_id,
+        #         )
+        #     )
+        #     if not quiz_field:
+        #         return None
+        #
+        #     res: PermissionId | None = (
+        #         await self.check_permission_for_edit_contest(
+        #             user_id=user_id,
+        #             contest_id=quiz_field.contest_id,
+        #         )
+        #     )
+        #     return res
 
     async def check_permission_for_edit_problem_card(
             self,
             user_id: int,
             problem_card_id: int,
     ) -> PermissionId | None:
-        async with self.uow:
-            problem_card: ProblemCard = (
-                await self.uow.problem_card_repo.get_problem_card_by_id(
-                    problem_card_id=problem_card_id,
-                )
-            )
-            if not problem_card:
-                return None
-
-            res: PermissionId | None = (
-                await self.check_permission_for_edit_quiz_field(
-                    user_id=user_id,
-                    quiz_field_id=problem_card.quiz_field_id,
-                )
-            )
-            return res
+        raise ObsoleteException
+        # async with self.uow:
+        #     problem_card: ProblemCard = (
+        #         await self.uow.problem_card_repo.get_problem_card_by_id(
+        #             problem_card_id=problem_card_id,
+        #         )
+        #     )
+        #     if not problem_card:
+        #         return None
+        #
+        #     res: PermissionId | None = (
+        #         await self.check_permission_for_edit_quiz_field(
+        #             user_id=user_id,
+        #             quiz_field_id=problem_card.quiz_field_id,
+        #         )
+        #     )
+        #     return res
 
     async def check_permission_for_edit_problem(
             self,
             user_id: int,
             problem_id: int,
     ) -> PermissionId | None:
-        async with self.uow:
-            problem_card: ProblemCard = (
-                await self.uow.problem_card_repo.get_problem_card_by_problem_id(
-                    problem_id=problem_id,
-                )
-            )
-            if not problem_card:
-                return None
-
-            res: PermissionId | None = (
-                await self.check_permission_for_edit_problem_card(
-                    user_id=user_id,
-                    problem_card_id=problem_card.id,
-                )
-            )
-            return res
+        raise ObsoleteException
+        # async with self.uow:
+        #     problem_card: ProblemCard = (
+        #         await self.uow.problem_card_repo.get_problem_card_by_problem_id(
+        #             problem_id=problem_id,
+        #         )
+        #     )
+        #     if not problem_card:
+        #         return None
+        #
+        #     res: PermissionId | None = (
+        #         await self.check_permission_for_edit_problem_card(
+        #             user_id=user_id,
+        #             problem_card_id=problem_card.id,
+        #         )
+        #     )
+        #     return res
 
     async def check_permission_for_view_contest_standings(
             self,
             user_id: int,
             contest_id: int,
     ) -> PermissionPromise | None:
-        async with self.uow:
-            user: User | None = (
-                await self.uow.user_repo.get_user_by_id(
-                    user_id=user_id,
-                )
-            )
-            if not user:
-                raise EntityDoesNotExist("user not found")
-
-            contest: Contest | None = (
-                await self.uow.contest_repo.get_contest_by_id(
-                    contest_id=contest_id,
-                )
-            )
-            if not contest:
-                raise EntityDoesNotExist("contest not found")
-
-            if user.domain_number == 0:
-                permission: Permission | None = (
-                    await self.check_permission_for_edit_contest(
-                        user_id=user_id,
-                        contest_id=contest_id,
-                    )
-                )
-                if permission:
-                    return PermissionPromise()
-                return None
-            else:
-                if user.domain_number == contest.id:
-                    return PermissionPromise()
-                return None
+        raise ObsoleteException
+        # async with self.uow:
+        #     user: User | None = (
+        #         await self.uow.user_repo.get_user_by_id(
+        #             user_id=user_id,
+        #         )
+        #     )
+        #     if not user:
+        #         raise EntityDoesNotExist("user not found")
+        #
+        #     contest: Contest | None = (
+        #         await self.uow.contest_repo.get_contest_by_id(
+        #             contest_id=contest_id,
+        #         )
+        #     )
+        #     if not contest:
+        #         raise EntityDoesNotExist("contest not found")
+        #
+        #     if user.domain_number == 0:
+        #         permission: Permission | None = (
+        #             await self.check_permission_for_edit_contest(
+        #                 user_id=user_id,
+        #                 contest_id=contest_id,
+        #             )
+        #         )
+        #         if permission:
+        #             return PermissionPromise()
+        #         return None
+        #     else:
+        #         if user.domain_number == contest.id:
+        #             return PermissionPromise()
+        #         return None
--- backend/core/services/providers/permission.py
+++ backend/core/services/providers/permission.py
@@ -6,11 +6,14 @@ from backend.core.repository.crud.uow import (
 )
 from backend.core.services.domain.permission import PermissionService
 from backend.core.services.interfaces.permission import IPermissionService
+from backend.core.utilities.exceptions.handlers.development import ObsoleteException
 
 
 def get_permission_service(
         uow: UnitOfWork = Depends(get_unit_of_work),
 ) -> IPermissionService:
-    return PermissionService(
-        uow=uow,
-    )
+    raise ObsoleteException
+
+    # return PermissionService(
+    #     uow=uow,
+    # )
