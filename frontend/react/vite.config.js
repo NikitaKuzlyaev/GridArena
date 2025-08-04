c2a46bc467dfc388e8ef735de5b2adb579b68945
Nikita <kuzliaev.nikita@yandex.ru>
2025-08-04 12:33:53 +0700

    perf(frontend): Remove results of some third-party api call to the server side

--- frontend/react/vite.config.js
+++ frontend/react/vite.config.js
@@ -8,5 +8,28 @@ export default defineConfig({
     alias: {
       '@': '/src'
     }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          // Оптимизация для шрифтов - добавляем хэш для кэширования
+          if (assetInfo.name && assetInfo.name.endsWith('.woff2')) {
+            return 'assets/fonts/[name]-[hash][extname]'
+          }
+          if (assetInfo.name && assetInfo.name.endsWith('.woff')) {
+            return 'assets/fonts/[name]-[hash][extname]'
+          }
+          return 'assets/[name]-[hash][extname]'
+        }
+      }
+    }
+  },
+  server: {
+    // Настройки для разработки
+    headers: {
+      'Cache-Control': 'public, max-age=31536000, immutable'
+    }
   }
 }) 
+
